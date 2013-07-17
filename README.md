9b68d3198a3b0f4ea6ce6c4124cccc9c3eeb7d9f
Arpan Chavda <arpanchavdaeng@gmail.com>
2013-07-17 10:31:55 +0530

    print fix and update readme

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ web_fetcher_python
 ==================
 
 Personal HTML Parser using BeautifulSoup
+
+Prerequisites 
+=============
+
+- Python 2
+- BeautifulSoup 4
+
--- halfmantr.py
+++ halfmantr.py
@@ -5,7 +5,7 @@ from BeautifulSoup import BeautifulSoup
 import re
 import urllib2 
 # Copy all of the content from the provided web page
-webpage = urlopen('http://www.halfmantr.com/final-countdown-list').read()
+webpage = urlopen('http://www.halfmantr.com/environment').read()
  
 # Grab everything that lies between the title tags using a REGEX
 #patFinderTitle = re.compile('')
@@ -19,25 +19,27 @@ webpage = urlopen('http://www.halfmantr.com/final-countdown-list').read()
 #create list iterator
 listIterator = []
  
-listIterator[:] = range(0,5)
+listIterator[:] = range(0,50)
  
 soup2 = BeautifulSoup(webpage)
 
  
 #titleSoup = soup2.findAll("title")
  
-linkSoup = soup2.findAll("a" , "yjns_rm")
+linkSoup = soup2.findAll("a" , "yjnewsflash_title")
 #fetching links
 
 output = [ x["href"] for x in linkSoup ]
 #fetching href(url) attribute value
  
 for i in listIterator:
-       
+      
+      #print "Fetching article http://www.halfmantr.com" + output[i]
       pageFile = urllib2.urlopen("http://www.halfmantr.com" + output[i])
       pageHtml = pageFile.read()
       pageFile.close()
       soup = BeautifulSoup("".join(pageHtml)) 
       sAll = soup.findAll("div","newsitem_text")
-      print sAll
+      for member in sAll:
+         print member
 
